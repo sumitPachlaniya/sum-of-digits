e6d8f883a3188274eaa48d3db84fd92f7c1cd462
sumitPachlaniya <97600179+sumitPachlaniya@users.noreply.github.com>
2022-01-15 21:36:49 +0530

    Add files via upload

---
+++ playground (11).swift
@@ -0,0 +1,6 @@
+func sum (firstDigit: Int , secondDigit: Int , thirdDigit: Int ,fourthDigit: Int ) -> Int{
+    let result = firstDigit + secondDigit + thirdDigit + fourthDigit
+    return result
+}
+var rst = sum(firstDigit : 1225, secondDigit : 3120,  thirdDigit : 4320, fourthDigit : 5320)
+print(rst)
